fca319c8bb664908a3012e17992106a0ddbe127f
Jacek Chmielewski <jchmielewski@teonite.com>
2020-11-14 09:46:25 +0100

    Add colored.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use scraper::{Html, Selector};
+use colored::*;
+
 static URL: &str = "http://status.hirezstudios.com/";
 
+fn color_status(status: &str) -> ColoredString {
+    // FIXME: can we use smart pattern-matching here?
+    let status_lower = status.to_lowercase();
+    if status_lower.contains("operational") {
+        status.bright_green()
+    } else if status_lower.contains("maintenance") {
+        status.bright_blue()
+    } else if status_lower.contains("degraded") {
+        status.bright_yellow()
+    } else {
+        status.bright_red()
+    }
+}
 fn main() {
     let document = reqwest::blocking::get(URL)
         .expect("Failed to fetch Hi-Rez status website.")
@@ -15,6 +30,6 @@ fn main() {
         let group_name = _group_name.trim();
         let _status = group.select(&status_selector).next().unwrap().inner_html();
         let status = _status.trim();
-        println!("{:20} {}", group_name, status);
+        println!("{:20} {}", group_name, color_status(status));
     } 
 }
