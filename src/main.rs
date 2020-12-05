7959a34adbe107e8d3024d7b998dac445895ed79
Jacek Chmielewski <jchmielewski@teonite.com>
2020-12-05 06:19:06 +0100

    Anyhow error handling, colored help.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use clap::{App, Arg, ArgMatches};
+use clap::{App, Arg, ArgMatches, AppSettings};
 use colored::*;
 use scraper::{ElementRef, Html, Selector};
+use anyhow::{Context, Result};
 static URL: &str = "http://status.hirezstudios.com/";
 
 fn color_status(status: &str) -> ColoredString {
@@ -19,6 +20,7 @@ fn color_status(status: &str) -> ColoredString {
 
 fn configure_clap<'a>() -> ArgMatches<'a> {
     App::new("hirez")
+        .global_settings(&[AppSettings::ColoredHelp])
         .author("Jacek Chmielewski <jchmielewski@teonite.com>")
         .version(env!("CARGO_PKG_VERSION"))
         .args(&[
@@ -31,43 +33,44 @@ fn configure_clap<'a>() -> ArgMatches<'a> {
         .get_matches()
 }
 
-fn text_from_selector(element: &ElementRef, selector: &str) -> String {
+fn text_from_selector(element: &ElementRef, selector: &str) -> Result<String> {
     let untrimmed = element
         .select(&Selector::parse(selector).unwrap())
         .next()
-        .unwrap()
+        .context(format!("Failed to create selector: {}", selector))?
         .inner_html();
-    String::from(untrimmed.trim())
+    Ok(String::from(untrimmed.trim()))
 }
 
-fn main() {
+fn main() -> Result<()> {
     let matches = configure_clap();
     let document = reqwest::blocking::get(URL)
-        .expect("Failed to fetch Hi-Rez status website.")
+        .context("Failed to fetch Hi-Rez status website.")?
         .text()
-        .unwrap();
+        .context("Failed to parse Hi-Rez status website.")?;
     let parsed = Html::parse_document(&document);
     let group_selector = Selector::parse("div.component-container.is-group").unwrap();
     let platform_selector =
         Selector::parse("div.child-components-container > div.component-inner-container").unwrap();
     println!();
     for group in parsed.select(&group_selector) {
-        let group_name = text_from_selector(&group, "span.name > span:nth-child(2)");
+        let group_name = text_from_selector(&group, "span.name > span:nth-child(2)")?;
         if let Some(game) = matches.value_of("game") {
             if !group_name.to_lowercase().contains(&game.to_lowercase()) {
                 continue;
             }
         }
         if matches.is_present("compact") {
-            let status = text_from_selector(&group, "span.component-status");
+            let status = text_from_selector(&group, "span.component-status")?;
             println!("{:20} {}", group_name, color_status(&status));
         } else {
             for platform in group.select(&platform_selector) {
-                let platform_name = text_from_selector(&platform, "span.name");
-                let platform_status = text_from_selector(&platform, "span.component-status");
+                let platform_name = text_from_selector(&platform, "span.name")?;
+                let platform_status = text_from_selector(&platform, "span.component-status")?;
                 println!("  {:20} {}", platform_name, color_status(&platform_status));
             }
             println!();
         }
     }
+    Ok(())
 }
